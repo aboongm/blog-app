23f60a42239595f3d10db4652cc8479d97273a23
aboongm <aboongm@yahoo.com>
2022-07-13 17:24:30 +0530

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, class_name: 'comment', foreign_key: 'author_id'
   has_many :likes, class_name: 'like', foreign_key: 'author_id'
 
-  def recent_posts
+  def self.recent_posts (posts)
     posts.order(created_at: :desc).limit(3)
   end
 end
