e2770c714b86162d085416eb2cd6511ed4c67cfa
aboongm <aboongm@yahoo.com>
2022-07-13 17:25:13 +0530

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  
   after_save :update_comments_counter
 
-  def update_comments_counter
+  def self.update_comments_counter(post)
     post.increment!(:comments_counter)
   end
 end
