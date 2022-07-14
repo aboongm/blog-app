aa6e1779e281c0e06cfb962fc4e94bff101f2ac0
aboongm <aboongm@yahoo.com>
2022-07-14 11:20:40 +0530

    Create unittest for like class

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,7 +3,7 @@ class Like < ApplicationRecord
   belongs_to :post
   after_save :update_likes_counter
 
-  def self.update_likes_counter(post)
+  def update_likes_counter
     post.increment!(:likes_counter)
   end
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
+RSpec.describe Like, type: :model do
   before(:all) do
     @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
 
     @post = Post.new(title: 'Post Title', text: 'Post text', likes_counter: 0, comments_counter: 0, author: @user)
   end
 
-  it 'should return comments_counter incremented by 1' do
+  it 'should return likes_counter incremented by 1' do
     @post.save
-    @comment = Comment.new(text: 'Comment text', author: @user, post: @post)
-    @comment.save
-    expect(@post.comments_counter).to eq(1)
+    @like = Like.new(author: @user, post: @post)
+    @like.save
+    expect(@post.likes_counter).to eq(1)
   end
 end
