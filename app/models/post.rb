2d10f43b0642eff65e5865c9ef12dd4338ba9191
aboongm <aboongm@yahoo.com>
2022-07-14 08:48:31 +0530

    Update post class to include validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, allow_blank: false
+  validates :text, presence: true, allow_blank: false
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   belongs_to :author, class_name: 'User'
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
