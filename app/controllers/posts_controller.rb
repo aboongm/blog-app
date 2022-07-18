77e22ae5fc7c6fdf11b566976e4f004628b12502
aboongm <aboongm@yahoo.com>
2022-07-18 17:10:26 +0530

    Corrections for linting errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
-    def index
-        @user = User.find(params[:user_id])
-    end
+  def index
+    @user = User.find(params[:user_id])
+  end
 
-    def show
-        @post = Post.find(params[:id])
-    end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
