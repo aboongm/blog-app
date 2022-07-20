d1ebab188ebb2ad03d5c939c86c15568b5defbf6
aboongm <aboongm@yahoo.com>
2022-07-20 21:55:00 +0530

    Create new and create methods in posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,18 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
+    # @user = User.find(params[:user_id])
+    @user = current_user
     @posts = @user.posts
+  end
+
+  def new
+    @post = Post.new
+    @user = current_user
+  end
+
+  def create
+    Post.create(post_params)
+    redirect_to { user_posts_path(current_user) }
   end
 
   def show
