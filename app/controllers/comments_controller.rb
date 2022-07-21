79bc1ac01691ed77c0191d5508d285577a3c7306
aboongm <aboongm@yahoo.com>
2022-07-21 16:34:34 +0530

    Update comments_controller and new html file for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,9 @@ class CommentsController < ApplicationController
     end
 
     def create
-        @comment = Comment.new(comment_params)
+        @post = Post.find(params[:post_id] || params[:id])
+        @comment = Comment.new(text: params[:comment][:text], post_id: @post.id, author_id: current_user.id)
         @comment.author_id = current_user.id
-        @comment.post_id = params[:post_id]
-        @post = Post.find(params[:post_id])
 
         respond_to do |format|
             if @comment.save
@@ -16,6 +15,10 @@ class CommentsController < ApplicationController
             else
                 format.html { render :new, status: :unprocessable_entity }
             end
+        end
+
+        def comment_params
+            params.require(:comment).permit(:text)
         end
     end
 end
